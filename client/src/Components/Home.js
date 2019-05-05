9e6988f86fa4baa36405d8e66aa707c415db95f7
Clint Masters <clintcmasters@gmail.com>
2019-05-04 20:29:21 -0500

    Fixed heroku issue

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
-import {getLocation, getPrice, getDistance, getTerm} from './functions';
+import { getLocation, getPrice, getDistance, getTerm } from "./functions";
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = { businesses: [], term: '', location: '', price: '', distance: '' };
+    this.state = {
+      businesses: [],
+      term: "",
+      location: "",
+      price: "",
+      distance: ""
+    };
     this.handleChangeTerm = this.handleChangeTerm.bind(this);
     this.handleChangeLocation = this.handleChangeLocation.bind(this);
     this.handleChangePrice = this.handleChangePrice.bind(this);
@@ -11,44 +17,52 @@ class Home extends Component {
   }
 
   handleChangeTerm(event) {
-    this.setState({term: event.target.value});
+    this.setState({ term: event.target.value });
   }
 
   handleChangeLocation(event) {
-    this.setState({location: event.target.value});
+    this.setState({ location: event.target.value });
   }
 
   handleChangeDistance(event) {
-    this.setState({distance: event.target.value});
+    this.setState({ distance: event.target.value });
   }
 
   handleChangePrice(event) {
-    this.setState({price: event.target.value});
+    this.setState({ price: event.target.value });
   }
 
   getFood = () => {
-    var params = {price: this.state.price, distance: this.state.distance, term: this.state.term}
-    if(params.price===''){
-        params.price='Inconceivable';
+    var params = {
+      price: this.state.price,
+      distance: this.state.distance,
+      term: this.state.term
+    };
+    if (params.price === "") {
+      params.price = "Inconceivable";
     }
-    if(params.distance===''){
-        params.distance='Inconceivable';
+    if (params.distance === "") {
+      params.distance = "Inconceivable";
     }
-    if(params.term===''){
-        params.term='Inconceivable';
+    if (params.term === "") {
+      params.term = "Inconceivable";
     }
-    fetch("/api/findFood/"+this.state.location+"/"
-    +params.term+"/"+params.distance+"/"+params.price)
-    .then(function(response) {
-        console.log(response);
-        console.log(response.text());
-        return response.json();
-      })
-      .then(function(body) {
+    fetch(
+      "/api/findFood/" +
+        this.state.location +
+        "/" +
+        params.term +
+        "/" +
+        params.distance +
+        "/" +
+        params.price
+    )
+      .then(res => res.json())
+      .then(body =>
         body.businesses
           ? this.setState({ businesses: body.businesses })
           : this.setState({ businesses: [] })
-      });
+      );
   };
 
   render() {
@@ -80,15 +94,17 @@ class Home extends Component {
             <h1>Search</h1>
             Term: &nbsp; {getTerm(this.state, this.handleChangeTerm)}
             <br />
-            Location: &nbsp; {getLocation(this.state, this.handleChangeLocation)}
+            Location: &nbsp;{" "}
+            {getLocation(this.state, this.handleChangeLocation)}
             <br />
             Price: &nbsp; {getPrice(this.state, this.handleChangePrice)}
             <br />
-            Distance: &nbsp; {getDistance(this.state, this.handleChangedistance)}
+            Distance: &nbsp;{" "}
+            {getDistance(this.state, this.handleChangedistance)}
             <br />
             <button className="more" onClick={this.getFood}>
-                Get Food
-              </button>
+              Get Food
+            </button>
           </div>
         </div>
       </div>
